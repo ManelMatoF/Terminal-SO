10b2ae65f674023b20f0fee0a5f7e433e45d48c2
ManelMatoF <129686538+ManelMatoF@users.noreply.github.com>
2023-09-20 10:42:47 +0200

    Update lista.h

--- lista.h
+++ lista.h
@@ -1,1 +1,20 @@
+#ifndef TERMINAL_SHELL_H
+#define TERMINAL_SHELL_H
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h> // Para la función open
+#include <unistd.h> // Para la función close
+#define MAXNAME 100
+typedef struct FileInfo* tPosL;
+struct FileInfo{
+    int modo;
+    int df;
+    char name[MAXNAME];
+    tPosL next;
+};
+typedef tPosL listFiles;
+
+
+#endif //TERMINAL_SHELL_H
 
