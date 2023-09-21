8b4bd061839009ae00d6481e59fd8b347d7b6f71
ferlosaada <145337559+ferlosaada@users.noreply.github.com>
2023-09-21 13:45:56 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #define MAX 100
 
@@ -8,7 +9,7 @@ void imprimirPromt(){
 }
 
 void leerEntrada(char * input){
-    fgets(input, 100, stdin);
+    fgets(input, MAX, stdin);//cuantos caracteres leer (preguntar)
 }
 
 int TrocearCadena(char *cadena, char *trozos[]){
@@ -20,29 +21,33 @@ int TrocearCadena(char *cadena, char *trozos[]){
     return i;
 }
 
-void procesarEntrada(char *input_trozos[], int num_input, int terminado){
-    switch (*input_trozos[0]){
-        if(strcmp(*input_trozos[0], "authors\0"))
-            printf("hola");
-        else if(strcmp(*input_trozos[0], "date\0"))
-            printf("adios");
-        else
-            printf("Opcion no valida\n");
-    }
-    
+void procesarEntrada(char *cadena, char *input_trozos[], int num_input, int terminado){//Lista de comandos con switch o if else (preguntar)
+    TrocearCadena(cadena, input_trozos);
+    if(strcmp(input_trozos[0], "authors") == 0)
+        printf("hola\n");
+    else if(strcmp(input_trozos[0], "date") == 0)
+        printf("adios\n");
+    else
+        printf("Opcion no valida\n");
 }
 
 
 int main(){
-    char * input; 
-    char * input_trozos[MAX];
+    char input[MAX]; //preguntar si asi o con el malloc (punteros)
+    char *input_trozos[10];
     int num_input, terminado;
 
+    for (int i = 0; i < 10; i++) {
+        input_trozos[i] = malloc(MAX * sizeof(char));  // Allocate memory for each string in input_trozos
+    }
     terminado = 0;
 
     while(!terminado){
         imprimirPromt();
         leerEntrada(input);
-        procesarEntrada(input_trozos, num_input, terminado);
+        procesarEntrada(input, input_trozos, num_input, terminado);
     }
-}
+
+    for (int i = 0; i < 10; i++) {
+        free(input_trozos[i]);
+    }
