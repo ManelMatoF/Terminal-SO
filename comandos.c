a28e8682b5f955c999e83880737990cd54848c44
ManelMatoF <129686538+ManelMatoF@users.noreply.github.com>
2023-09-22 08:45:19 +0200

    Update comandos.c

--- comandos.c
+++ comandos.c
@@ -141,8 +141,55 @@ void Cmd_open (char * tr[], listFiles *L) {
 
 
 void authors(char *input_trozos[]){
-   if(strcmp(input_trozos[1], "-n") == 0)
-      printf("");
-   else if(strcmp(input_trozos[1], "-l") == 0)
+    if(strcmp(input_trozos[1], "-n") == 0)
+        printf("fernado.losada@udc.es\nmanel.mfernandez@udc.es\n");
+    
+    else if(strcmp(input_trozos[1], "-l") == 0)
+        printf("Fernando Losada Perez\nManel Mato Fernandez\n");
+    else printf("fernado.losada@udc.es: Fernando Losada Perez\nmanel.mfernandez@udc.es: Manel Mato Fernandez\n");
+}
+
+void date(){
+    struct tm *fecha_aux;
+    time_t fecha;
+    time(&fecha);
+    fecha_aux = localtime(&fecha);
+
+    printf("%02d/%02d/%02d",fecha_aux->tm_mday, fecha_aux->tm_mon, fecha_aux->tm_year);
+}
+
+void tiempo(){
+    time_t hora;
+    struct tm *time_aux;
+    time(&hora);
+    time_aux = localtime(&hora);
+
+    printf("%02d:%02d:%02d",time_aux->tm_hour, time_aux->tm_min, time_aux->tm_sec);
+}
+
+void infosys(){
+    struct utsname info_sys;
+    if (uname(&info_sys) != -1){
+        printf("Informacion  de la maquina %s\n", info_sys.machine);
+        printf("Host Name: %s\n", info_sys.nodename);
+        printf("Version del Kernel: %s\n", info_sys.release);
+        printf("Sistema Operativo: %s\n", info_sys.sysname);
+        printf("Version del sistema: %s\n", info_sys.version);
+
+    } else {
+        perror("No se ha podido obtener la informacion del sistema");
+    }
+}
+
+void quit(){
+exit(0);
+}
+
+void exit_func(){
+exit(0);
+}
+
+void bye(){
+exit(0);
 }
    
