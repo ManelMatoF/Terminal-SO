940e1425b6d982fc1aace47fbce90c6fb5df9727
ferlosaada <145337559+ferlosaada@users.noreply.github.com>
2023-09-29 11:03:36 +0200

    Update comandos.h

--- comandos.h
+++ comandos.h
@@ -1,18 +1,8 @@
 #ifndef P0_COMANDOS_H
 #define P0_COMANDOS_H
 
-#include <string.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h> // Para la función open
-#include <unistd.h> // Para la función close
-#include <time.h>
 #include <stdbool.h>
-#include <ctype.h>
-#include <sys/types.h>
-#include <sys/utsname.h>
-#include <errno.h>
-#include <limits.h>
 #include "lista.h"
 
 #define MAXNAME 100
@@ -28,12 +18,12 @@ void tiempo();
 void infosys();
 void pid(char *input_trozos[]);
 void chdir_func(char *input_trozos[]);
-void repeat_command(char *input_trozos[], listHist H);
-void hist(char *input_trozos[]);
+bool *repeat_command(char *input_trozos[], listHist H, char *cadena);
+void hist(char *input_trozos[], listHist *H);
 void help(char *commands[], char *input_trozos[], int nComands);
-void quit(bool terminado);
-void exit_func(bool terminado);
-void bye(bool terminado);
+bool quit();
+bool exit_func();
+bool bye();
 
 
 #endif //P0_COMANDOS_H
