8e9c4aca035073d93f436779d53bf19bab3d8138
ferlosaada <145337559+ferlosaada@users.noreply.github.com>
2023-09-29 09:45:59 +0200

    Update comandos.h

--- comandos.h
+++ comandos.h
@@ -1,16 +1,21 @@
-#ifndef TERMINAL_SHELL_H
-#define TERMINAL_SHELL_H
+#ifndef P0_COMANDOS_H
+#define P0_COMANDOS_H
+
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h> // Para la función open
 #include <unistd.h> // Para la función close
+#include <time.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <sys/types.h>
+#include <sys/utsname.h>
+#include <errno.h>
+#include <limits.h>
+#include "lista.h"
+
 #define MAXNAME 100
-struct FileInfo{
-    int modo;
-    int df;
-    char name[MAXNAME];
-    struct FileInfo* next;
-};
-struct FileInfo* listFiles = NULL;
-#endif //TERMINAL_SHELL_H
+
+
+#endif //P0_COMANDOS_H
