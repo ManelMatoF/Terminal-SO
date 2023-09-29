2a8141af2929e4e352216e2d0e35960c6a5ba6e4
ManelMatoF <129686538+ManelMatoF@users.noreply.github.com>
2023-09-29 10:17:10 +0200

    Update lista.h

--- lista.h
+++ lista.h
@@ -21,9 +21,22 @@ typedef tPosL listFiles;
 
 typedef struct ComandHist* tPosH;
 struct ComandHist{
-    char comand[MAXNAME];
+    char name[MAXNAME];
     tPosH next;
 };
 typedef tPosH listHist;
 
+void createEmptyListF(listFiles *L);
+void createEmptyListH(listHist *L);
+tPosL lastF(listFiles L);
+tPosH lastH(listHist L);
+tPosL previousF(tPosL p, listFiles L);
+tPosH previousH(tPosH p, listHist L);
+int countFiles(listFiles *L);
+char *getItemF(tPosL p,listFiles L);
+tPosL findItemF(int df, listFiles L);
+void insertItemF(int df, int mode, char name[MAXNAME], listFiles *L);
+void insertItemH(char name[MAXNAME], listHist *L);
+void deleteItemF(listFiles *L, int df);
+void deleteListH(listHist *L);
 #endif
