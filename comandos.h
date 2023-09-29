959d707ef0bedb812ab767456585feebe5becc27
ManelMatoF <129686538+ManelMatoF@users.noreply.github.com>
2023-09-29 10:17:38 +0200

    Update comandos.h

--- comandos.h
+++ comandos.h
@@ -18,4 +18,22 @@
 #define MAXNAME 100
 
 
+void ListOpenFiles(listFiles L);
+void Cmd_open (char * tr[], listFiles *L);
+void Cmd_close (char *tr[], listFiles *L);
+void Cmd_dup (char * tr[], listFiles *L);
+void authors(char *input_trozos[]);
+void date();
+void tiempo();
+void infosys();
+void pid(char *input_trozos[]);
+void chdir_func(char *input_trozos[]);
+void repeat_command(char *input_trozos[], listHist H);
+void hist(char *input_trozos[]);
+void help(char *commands[], char *input_trozos[], int nComands);
+void quit(bool terminado);
+void exit_func(bool terminado);
+void bye(bool terminado);
+
+
 #endif //P0_COMANDOS_H
