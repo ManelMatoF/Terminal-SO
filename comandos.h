639205a5791e5ebb60cf89dfb8396202ad8d6da8
ferlosaada <145337559+ferlosaada@users.noreply.github.com>
2023-10-06 13:12:07 +0200

    Update comandos.h

--- comandos.h
+++ comandos.h
@@ -5,20 +5,20 @@
 #include <stdbool.h>
 #include "lista.h"
 
-#define MAXNAME 100
+#define MAXNAME 150
 
-void ListOpenFiles(listFiles L);
-void Cmd_open (char * tr[], listFiles *L);
-void Cmd_close (char *tr[], listFiles *L);
-void Cmd_dup (char * tr[], listFiles *L);
+void ListOpenFiles(tList L);
+void Cmd_open (char * tr[], tList *L);
+void Cmd_close (char *tr[], tList *L);
+void Cmd_dup (char * tr[], tList *L);
 void authors(char *input_trozos[], int n);
 void date();
 void tiempo();
 void infosys();
 void pid(char *input_trozos[], int n);
 void chdir_func(char *input_trozos[], int n);
-bool repeat_command(char *input_trozos[], int n, listHist H, char *cadena, int *bucle);
-void hist(char *input_trozos[], int n, listHist *H);
+bool repeat_command(char *input_trozos[], int n, tList H, char *cadena, int *bucle);
+void hist(char *input_trozos[], int n, tList *H);
 void help(char *commands[], char *input_trozos[], int n, int nComands);
 void quit(bool *terminado);
 void exit_func(bool *terminado);
