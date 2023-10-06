4744ad0681cef63ad1a6124b501fd8343a5eed0a
ferlosaada <145337559+ferlosaada@users.noreply.github.com>
2023-10-06 13:11:06 +0200

    Update lista.h

--- lista.h
+++ lista.h
@@ -1,43 +1,40 @@
 #ifndef P0_LISTA_H
 #define P0_LISTA_H
-#define MAXNAME 100
+#define MAXNAME 150
 #include <stdio.h>
 #include <stdbool.h>
 
-typedef struct FileInfo* tPosF;
-struct FileInfo{
+
+typedef struct nodo *tPosL;
+struct nodo {
+    void *data;             
+    struct nodo *next;
+};
+typedef tPosL tList;
+
+
+void createEmptyList(tList *L);
+
+tPosL first(tList L);
+
+tPosL last(tList L);
+
+tPosL previous(tPosL p, tList L);
+
+tPosL next(tPosL p);
+
+void *getItem(tPosL p);
+
+bool insertItem(void *data, size_t dataSize, tList *L);
+
+bool deleteItem(tPosL p, tList *L);
+
+void deleteList(tList *L);
+
+typedef struct{
     int modo;
     int df;
     char name[MAXNAME];
-    tPosF next;
-};
-typedef tPosF listFiles;
-
-typedef struct ComandHist* tPosH;
-struct ComandHist{
-    char comand[MAXNAME];
-    tPosH next;
-};
-typedef tPosH listHist;
-
-void createEmptyListF(listFiles *L);
-void createEmptyListH(listHist *L);
-tPosF firstF(listFiles L);
-tPosH firstH(listHist L);
-tPosF lastF(listFiles L);
-tPosH lastH(listHist L);
-tPosF previousF(tPosF p, listFiles L);
-tPosH previousH(tPosH p, listHist L);
-tPosF nextF(tPosF p, listFiles L);
-tPosH nextH(tPosH p, listHist L);
-int countFiles(listFiles *L);
-char *getItemF(tPosF p, listFiles L);
-char *getItemH(tPosH p, listHist L);
-tPosF findItemF(int df, listFiles L);
-void insertItemF(int df, int mode, char name[MAXNAME], listFiles *L);
-void insertItemH(char name[MAXNAME], listHist *L);
-void deleteItemF(listFiles *L, int df);
-void deleteListH(listHist *L);
-void deleteListF(listFiles *L);
+}FileInfo;
 
 #endif
